f716e3040af9548aa951c03ca483f9a10f6ce5bd
lalahaha323 <2665687186@qq.com>
2018-11-18 19:56:01 +0800

    add my first java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+/*************************************************************************
+	> File Name: Hello.java
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年11月18日 星期日 19时55分17秒
+ ************************************************************************/
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
