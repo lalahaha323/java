dc049c4a8712e9305a5e96d6ed07f7a29be63e47
lalahaha323 <2665687186@qq.com>
2018-11-21 10:24:52 +0800

    The first java program

---
+++ MyFirstApp.java
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: MyFirstApp.java
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年11月20日 星期二 21时24分06秒
+ ************************************************************************/
+
+public class MyFirstApp {
+    public static void main(String[] args) {
+        System.out.println("I Rule!");
+        System.out.println("The World!");
+    }
+}
