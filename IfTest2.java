bbcfd2a5f689e62f15778c8de0d2856838bb396f
lalahaha323 <2665687186@qq.com>
2018-11-21 10:22:29 +0800

    If test 2

---
+++ IfTest2.java
@@ -0,0 +1,18 @@
+/*************************************************************************
+	> File Name: IfTest2.java
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年11月20日 星期二 21时32分18秒
+ ************************************************************************/
+
+class IfTest2 {
+    public static void main(String[] args) {
+        int x = 2;
+        if (x == 3) {
+            System.out.println("x must be 3");
+        } else {
+            System.out.println("x is NOT 3");
+        }
+        System.out.println("This runs no matter what");
+    }
+}
