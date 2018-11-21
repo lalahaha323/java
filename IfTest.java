d59996c163624ef1671ba468e146fb0917784208
lalahaha323 <2665687186@qq.com>
2018-11-21 10:22:08 +0800

    If test 1

---
+++ IfTest.java
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: IfTest.java
+	> Author: 
+	> Mail: 
+	> Created Time: 2018年11月20日 星期二 21时30分32秒
+ ************************************************************************/
+
+class IfTest {
+    public static void main(String[] args) {
+        int x = 3;
+        if (x == 3) {
+            System.out.println("x must be 3");
+        }
+        System.out.println("This runs no matter what");
+    }
+}
