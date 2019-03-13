e15fc4c09764dd9cb7f95af858d1c8ff17c68fb9
lalahaha323 <2665687186@qq.com>
2019-03-13 19:55:21 +0800

    test this code!

---
+++ BRRead.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class BRRead {
+    public static void main(String[] args) throws IOException {
+        char c;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("输入字符, 按下 'q' 键退出。");
+        do {
+            System.out.println("lala");
+            c = (char) br.read();
+            System.out.println(c);
+        } while (c != 'q');
+    }
+}
