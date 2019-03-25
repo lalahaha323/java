efc4c72e3f7131803e38bb33f52d2dae4d2f268d
lalahaha323 <2665687186@qq.com>
2019-03-25 11:14:51 +0800

    add map

--- Test.java
+++ Test.java
@@ -1,18 +1,22 @@
 import java.util.*;
 public class Test {
     public static void main(String[] args) {
-        List l1 = new LinkedList();
-        List l2 = new LinkedList();
-        for(int i = 0; i <= 9; i++) {
-            l1.add("a" + i);
+        Map m1 = new HashMap();
+        Map m2 = new TreeMap();
+        m1.put("one", new Integer(1));
+        m1.put("two", new Integer(2));
+        m1.put("three", new Integer(3));
+        m2.put("A", new Integer(1));
+        m2.put("B", new Integer(2));
+        System.out.println(m1.size());
+        System.out.println(m1.containsKey("one"));
+        System.out.println(m1.containsValue(new Integer(1)));
+        if(m1.containsKey("two")) {
+            int i = ((Integer)m1.get("two"));
+            System.out.println(i);
         }
-        System.out.println(l1);
-        Collections.shuffle(l1);
-        System.out.println(l1);
-        Collections.reverse(l1);
-        System.out.println(l1);
-        Collections.sort(l1);
-        System.out.println(l1);
-        System.out.println(Collections.binarySearch(l1,"a5"));
+        Map m3 = new HashMap(m1);
+        m3.putAll(m2);
+        System.out.println(m3);
     }
 }
