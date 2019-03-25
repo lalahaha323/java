18dda5339a8948c43b3b16bfd0b010968c65c9c9
lalahaha323 <2665687186@qq.com>
2019-03-25 11:38:33 +0800

    add test

--- Test.java
+++ Test.java
@@ -1,20 +1,13 @@
 import java.util.*;
 public class Test {
+    private static final Integer ONE = new Integer(1);
     public static void main(String[] args) {
-        Map m1 = new HashMap();
-        Map m2 = new TreeMap();
-        m1.put("one", 1);
-        m1.put("two", 2);
-        m1.put("three", 3);
-        m2.put("A", 1);
-        m2.put("B", 2);
-        System.out.println(m1.size());
-        System.out.println(m1);
-        System.out.println(m1.containsKey("one"));
-        System.out.println(m2.containsValue(1));
-        if(m1.containsKey("two")) {
-            int i = (Integer)m1.get("two");
-            System.out.println(i);
+        Map m = new HashMap();
+        for(int i = 0; i < args.length; i++) {
+            Integer freq = (Integer)m.get(args[i]);
+            m.put(args[i], (freq == null? ONE : new Integer(freq.intValue() + 1)));
         }
+        System.out.println(m.size() + " distinct words detected:");
+        System.out.println(m);
     }
 }
