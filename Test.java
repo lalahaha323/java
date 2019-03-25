682bae958c794549cc312802237d88b20a8e3fcc
lalahaha323 <2665687186@qq.com>
2019-03-25 10:40:08 +0800

    add test

--- Test.java
+++ Test.java
@@ -1,10 +1,18 @@
 import java.util.*;
 public class Test {
     public static void main(String[] args) {
-        Set s = new HashSet();
-        s.add("hello");
-        s.add("world");
-        s.add("world");
-        System.out.println(s);
+        List l1 = new LinkedList();
+        for(int i = 0; i <= 5; i++) {
+            l1.add("a" + i);
+        }
+        System.out.println(l1);
+        l1.add(3,"a100");
+        System.out.println(l1);
+        l1.set(6,"a200");
+        System.out.println(l1);
+        System.out.print((String)l1.get(2) + " ");
+        System.out.println(l1.indexOf("a3"));
+        l1.remove(1);
+        System.out.println(l1);
     }
 }
