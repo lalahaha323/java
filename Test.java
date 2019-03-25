b53dadb47d17f8e423ab9159902526365c8f0f91
lalahaha323 <2665687186@qq.com>
2019-03-25 10:51:09 +0800

    add

--- Test.java
+++ Test.java
@@ -2,17 +2,17 @@ import java.util.*;
 public class Test {
     public static void main(String[] args) {
         List l1 = new LinkedList();
-        for(int i = 0; i <= 5; i++) {
+        List l2 = new LinkedList();
+        for(int i = 0; i <= 9; i++) {
             l1.add("a" + i);
         }
         System.out.println(l1);
-        l1.add(3,"a100");
+        Collections.shuffle(l1);
         System.out.println(l1);
-        l1.set(6,"a200");
+        Collections.reverse(l1);
         System.out.println(l1);
-        System.out.print((String)l1.get(2) + " ");
-        System.out.println(l1.indexOf("a3"));
-        l1.remove(1);
+        Collections.sort(l1);
         System.out.println(l1);
+        System.out.println(Collections.binarySearch(l1,"a5"));
     }
 }
